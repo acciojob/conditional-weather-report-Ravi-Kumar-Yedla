7c7bc8f95a9b6c16fb29efd45d8668b2cd2a4315
Ravi-Kumar-Yedla <ravikumaryedla292@gmail.com>
2024-09-18 11:45:03 +0530

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,56 @@
 
-import React, { useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 // import WeatherDisplay from "./WeatherDisplay";
 import WeatherDisplay from "./weatherDisplay";
-import { getImportCode } from "css-loader/dist/utils";
 
 
+const def = {temperature : 0, conditions : "Cool"}
 
-function App() {
-  const [weather, setWeather] = useState({ temperature: 0, conditions: '' });
-getImportCode
+const App = () => {
+
+  const[data, setData] = useState(def)
+
+
   useEffect(() => {
-   
-    setWeather({ temperature: 25, conditions: 'Sunny' });
-  },[])
+    setTimeout(() => {
+      setData({ temperature: 25, conditions: "Sunny" })
+    }, 1000)
+  })
 
   return (
     <div>
-      <h1>Weather Application</h1>
-      {/* <WeatherDisplay temperature={weather.temperature} conditions={weather.conditions}/> */}
-     <WeatherDisplay weather = {weather} />
-      
+      <WeatherDisplay data={data} />
     </div>
-  );
+  )
 }
+
 export default App
+
+
+// import React, { useEffect,useState} from "react";
+// import './../styles/App.css';
+// // import WeatherDisplay from "./WeatherDisplay";
+// import WeatherDisplay from "./weatherDisplay";
+// import { getImportCode } from "css-loader/dist/utils";
+
+
+
+// function App() {
+//   const [weather, setWeather] = useState({ temperature: 0, conditions: '' });
+// getImportCode
+//   useEffect(() => {
+   
+//     setWeather({ temperature: 25, conditions: 'Sunny' });
+//   },[])
+
+//   return (
+//     <div>
+//       <h1>Weather Application</h1>
+//       {/* <WeatherDisplay temperature={weather.temperature} conditions={weather.conditions}/> */}
+//      <WeatherDisplay weather = {weather} />
+      
+//     </div>
+//   );
+// }
+// export default App
--- src/components/WeatherDisplay.js
+++ src/components/WeatherDisplay.js
@@ -1,27 +1,49 @@
-// import Bluebird from 'cypress/types/bluebird'
 import React from 'react'
 
+const WeatherDisplay = (props) => {
+    const { data } = props;
+
+    // Define style for temperature based on the temperature value
+    const tempStyle = {
+        color: data.temperature > 20 ? "red" : "blue"
+    };
+
+    return (
+        <div>
+            {/* Apply tempStyle to the temperature */}
+            <p>Temperature: <span style={tempStyle}>{data.temperature}</span></p>
+            <p>Conditions: {data.conditions}</p>
+        </div>
+    );
+};
+
+export default WeatherDisplay;
 
 
-   function WeatherDisplay(props){
-      const {weather} = props
+// // import Bluebird from 'cypress/types/bluebird'
+// import React from 'react'
 
-      const tempStyle = {
-        color : weather.temperature > 20 ? 'rgb(255, 0, 0)' : 'blue',
-      };
+
+
+//    function WeatherDisplay(props){
+//       const {weather} = props
+
+//       const tempStyle = {
+//         color : weather.temperature > 20 ? 'rgb(255, 0, 0)' : 'blue',
+//       };
 
    
-  return (
-    <div>
-      <span>
-        {/* <p style={tempStyle}>Temperature: {temperature}</p>*/}
-        <p>Temperature: <span style={tempStyle}>{weather.temperature}</span></p>
-      <p>condtions: {weather.condition}</p>
-      </span>
+//   return (
+//     <div>
+//       <span>
+//         {/* <p style={tempStyle}>Temperature: {temperature}</p>*/}
+//         <p>Temperature: <span style={tempStyle}>{weather.temperature}</span></p>
+//       <p>condtions: {weather.condition}</p>
+//       </span>
 
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
 
-export default WeatherDisplay
+// export default WeatherDisplay
